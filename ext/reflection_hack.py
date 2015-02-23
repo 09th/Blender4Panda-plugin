050fa31ca621ddef51be4639bb6f1029728a0db1
09th <ruPanda3D@gmail.com>
2015-02-24 01:16:41 +0300

    First attemt to get environment mapping

---
+++ ext/reflection_hack.py
@@ -0,0 +1,125 @@
+import os, re
+
+order = 50
+target = 'material'
+
+def invoke(all_data, target_data, material, context, fname, flags=None):
+    tex_count = 0
+    color_tex_count = 0
+    color_tex_idx = 0
+    envmap = None
+    for tex in material.texture_slots:
+        if tex and tex.texture:
+            if tex.texture.type == 'IMAGE':
+                if tex.texture.image and tex.texture.image.source == 'FILE':
+                    tex_count += 1
+                    if tex.use_map_color_diffuse:
+                        color_tex_count += 1
+                        color_tex_idx = tex_count - 1
+            elif tex.texture.type == 'ENVIRONMENT_MAP':
+                if tex.texture.environment_map.source == 'IMAGE_FILE':
+                    if tex.texture.image and tex.texture.image.source == 'FILE':
+                        envmap = tex
+                        #break
+    if not envmap: # Exit here if envmap not found in texture slots
+        return False
+    
+    dirname = os.path.dirname(fname)
+    if 'paths' in all_data['scene']:
+        dirname = os.path.join(dirname, all_data['scene']['paths']['materials'])
+    vert_fname = os.path.join(dirname, target_data['vert'])
+    frag_fname = os.path.join(dirname, target_data['frag'])
+    
+    f = open(vert_fname, 'r')
+    vert = f.read()
+    f.close()
+    
+    f = open(frag_fname, 'r')
+    frag = f.read()
+    f.close()
+    
+    # --- Vertex shader ---
+    ins = '''
+// < insert by Reflection hack start (variables)
+uniform mat4 p3d_ViewMatrix; // world to view transformation
+uniform mat4 p3d_ViewMatrixInverse; // view to world transformation
+varying vec3 varreflect;
+// insert by Reflection hack end >
+'''
+    # Insert before first varying declaration
+    vert = re.sub('(varying[^;]+;)', ins + '\g<1>', vert, 1) 
+    
+    ins = '''
+    // < insert by Reflection hack start (calculation)
+    vec4 viewDirectionInViewSpace = co - vec4(0.0, 0.0, 0.0, 1.0);
+    vec3 viewDirection =  vec3(p3d_ViewMatrixInverse * viewDirectionInViewSpace);
+    vec3 normalDirection = normalize(vec3(vec4(varnormal, 0.0) * p3d_ViewMatrix));
+    varreflect = reflect(viewDirection, normalize(normalDirection));
+    // insert by Reflection hack end >
+'''
+    # Insert before last brace
+    vert = re.sub('(}[^\S]+$)', ins + '\g<1>', vert, 1)
+    
+    f = open(vert_fname, 'w')
+    f.write(vert)
+    f.close()
+    
+    # --- Fragment shader ---
+    ins = '''
+// < insert by Reflection hack start (variables)
+varying vec3 varreflect;
+uniform samplerCube cubemap;
+// insert by Reflection hack end >
+'''
+    # Insert before firs varying declaration
+    frag = re.sub('(varying[^;]+;)', ins + '\g<1>', frag, 1)
+    
+    # TODO: Check input colorspace option. Currently used default sRGB
+    ins = '''
+    // <-- insert by Reflection hack start
+    vec4 refl_tex = textureCube(cubemap, varreflect);
+    srgb_to_linearrgb(refl_tex, refl_tex);
+    mtex_rgb_blend(<color_tmp_in>, refl_tex.rgb, refl_tex.a, <blend_val>, <color_tmp_out>);
+    // insert by  Reflection hack end -->'''
+    ins = ins.replace('<blend_val>', str(envmap.diffuse_color_factor))
+    if color_tex_count > 0: 
+        # Mix with texture color if color texture was found
+        sr = re.findall('mtex_image\([\w]+, p3d_Texture%i[\s\S]+?mtex_rgb_blend\([^\)]+(tmp[0-9]+)\);' % (color_tex_idx), frag)
+        if sr:
+            color_tmp = sr[0]
+            ins = ins.replace('<color_tmp_in>', color_tmp).replace('<color_tmp_out>', color_tmp)
+            # Insert code after target texture blending
+            frag = re.sub('(mtex_rgb_blend\([^\)]+%s\);)' % color_tmp, '\g<1>'+ins, frag, 1)
+        else:
+            raise Exception('Not found color variable to inject reflection code')
+    else: 
+        # If no textures - mix with base diffuse color
+        sr = re.findall('(shade_madd\([ \S]+(cons[0-9]+), tmp[0-9]+\);)', frag)
+        if sr:
+            old_str = sr[0][0]
+            cons_name = sr[0][1]
+            new_str = old_str.replace(cons_name, 'refl_tex')
+            ins = ins.replace('<color_tmp_in>', cons_name+'.rgb').replace('<color_tmp_out>', 'refl_tex.rgb')
+            new_str = ins + '\n    ' + new_str
+            frag = frag.replace(old_str, new_str)
+        else:
+            raise Exception('Not found color variable to inject reflection code')
+        
+
+    f = open(frag_fname, 'w')
+    f.write(frag)
+    f.close()
+    
+    # --- Uniform ---
+    cm_types = {'IMAGE_FILE': 'image_cibemap',
+                'STATIC': 'static_cubemap',
+                'ANIMATED': 'dynamic_cubemap'}
+    
+    uniform = {'datatype': 1,
+               'type': cm_types[envmap.texture.environment_map.source],
+               'varname': "cubemap"
+                }
+    if envmap.texture.environment_map.source in ('STATIC', 'ANIMATED'):
+        uniform['object'] = ''
+    else:
+        uniform['image'] = ''
---
+++ ext/utils.py
@@ -0,0 +1,46 @@
+import os, bpy
+
+def convertFileNameToPanda(filename):
+  """ (Get from Chicken) Converts Blender filenames to Panda 3D filenames.
+  """
+  path =  filename.replace('//', './').replace('\\', '/')
+  if os.name == 'nt' and path.find(':') != -1:
+    path = '/'+ path[0].lower() + path[2:]
+  return path
+
+
+def save_image(img, file_path, text_path):
+    if img.filepath:
+        oldpath = bpy.path.abspath(img.filepath)
+        old_dir, old_f = os.path.split(convertFileNameToPanda(oldpath))       
+        f_names = [s.lower() for s in old_f.split('.')]
+        if not f_names[-1] in ('jpg', 'png', 'tga', 'tiff', 'dds', 'bmp') and img.is_dirty:
+            old_f += ('.' + bpy.context.scene.render.image_settings.file_format.lower())
+    else:
+        oldpath = ''
+        old_dir = ''
+        old_f = img.name + '.' + bpy.context.scene.render.image_settings.file_format.lower()
+    rel_path = os.path.join(text_path, old_f)
+    if os.name == 'nt':
+        rel_path = rel_path.replace(r"\\",r"/").replace('\\', '/')
+    new_dir, eg_f = os.path.split(file_path)
+    new_dir = os.path.abspath(os.path.join(new_dir, text_path))
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
+    if img.is_dirty or bool(img.packed_file):
+        try:
+            bpy.context.scene.render.image_settings.color_mode = 'RGBA'
+        except:
+            bpy.context.scene.render.image_settings.color_mode = 'RGB'
+        r_path = os.path.abspath(os.path.join(new_dir, old_f))
+        ext = bpy.context.scene.render.image_settings.file_format.lower()
+        r_path = os.path.splitext(r_path)[0] + '.' + ext
+        rel_path = os.path.splitext(rel_path)[0] + '.' + ext
+        img.save_render(r_path)
+        print('RENDER IMAGE to %s; rel path: %s' % (r_path, rel_path))
+    else:
+        newf = os.path.join(new_dir, old_f)
+        if oldpath != newf:
+            bpy_extras.io_utils.path_reference_copy(((oldpath.replace(r"\\", r"/"), newf),), report = print)
+            print('COPY IMAGE %s to %s; rel path %s' % (oldpath, newf, rel_path))
+    return rel_path
